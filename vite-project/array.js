290989c1f8c0e560abc2dee38b099811eeb07551
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-01-13 13:10:55 -0500

    s

--- vite-project/array.js
+++ vite-project/array.js
@@ -1,3 +1,3 @@
-try {
-    let api_url = "https://picsum.photos/v2/list?page=2&limit=100"
-}
+fetch('https://picsum.photos/v2/list?page=2&limit=100')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
